396dc242ae278fa46f2edcb93681fa3c1c3c6162
Victor Yang <pythonrocks@gmail.com>
2021-12-07 22:26:15 -0500

    WIP

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 INPUT_BUCKET=pso-victory-dev-8f039964-e2bb-11e8-b17e-1700de069414
 OUTPUT_BUCKET=pso-victory-dev-data
 
-gcloud beta functions deploy copier --runtime python37 \
+gcloud beta functions deploy copier --runtime python39 \
 --set-env-vars OUTPUT_BUCKET=${OUTPUT_BUCKET} \
 --trigger-resource ${INPUT_BUCKET} \
 --trigger-event google.storage.object.finalize \
